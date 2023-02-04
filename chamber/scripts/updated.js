978e6ddb0d2c73ba2d905d0222fd5d24133b86f9
andreyblanco <andygblanco@gmail.com>
2023-02-04 08:54:40 -0600

    meet

--- chamber/scripts/updated.js
+++ chamber/scripts/updated.js
@@ -6,7 +6,13 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 const year = now.getFullYear();
+const day = now.getDay();
 
 document.querySelector("#currentdate").textContent = `${fulldate}`;
 
 document.querySelector("#currentyear").textContent = `${year}`;
+
+if (day != 1 && day != 2)
+{
+	document.querySelector("#add").style.display = "none";
+}
