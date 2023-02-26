23dd1ba23c497fd1b3adee560782fa001a884013
andreyblanco <andygblanco@gmail.com>
2023-02-25 19:24:16 -0600

    lesson7

---
+++ lesson-7/scripts/scripts.js
@@ -0,0 +1,36 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+function loadImages(image){
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+  image.style.filter = "blur(0em)";
+  image.style.margin = "0px";
+  image.style.opacity = "1";
+  image.style.transition = "all 2s 1s";
+};
+
+if ("IntersectionObserver" in window) {
+  
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        console.log("aqui", item);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+
+} else {
+
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+
+  });
+}
