36747ec00f01fbf7aa8001ee1b809f0c9d51a65a
andreyblanco <andygblanco@gmail.com>
2023-02-26 17:21:10 -0600

    discovery page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -8,3 +8,70 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 
 x.onclick = toggleMenu;
+
+let imagesToLoad = document.querySelectorAll(".moveImg[data-src]");
+let boxToLoad = document.querySelectorAll(".box");
+
+function loadImages(image){
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+  image.style.filter = "blur(0em)";
+  image.style.margin = 0;
+  image.style.opacity = "1";
+  image.style.transition = "all 1s 0.5s";
+};
+
+function loadBox(box){
+    box.style.filter = "blur(0em)";
+    box.style.margin = 0;
+    box.style.opacity = "1";
+    box.style.transition = "all 2.5s 0.5s";
+  };
+
+if ("IntersectionObserver" in window) {
+  
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+
+
+} else {
+
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+
+}
+
+if ("IntersectionObserver" in window) {
+  
+    const observerBox = new IntersectionObserver((boxes, observer) => {
+      boxes.forEach((box) => {
+        if (box.isIntersecting) {
+          loadBox(box.target)
+          observer.unobserve(box.target);
+        }
+      });
+    });
+
+    boxToLoad.forEach((box) => {
+      observerBox.observe(box);
+    });
+  
+  } else {
+
+    boxToLoad.forEach((box) => {
+      loadBox(box);
+    });
+  }
--- chamber/scripts/updated.js
+++ chamber/scripts/updated.js
@@ -5,6 +5,10 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 
+const defdate = new Intl.DateTimeFormat("en-US").format(
+	now
+);
+
 const year = now.getFullYear();
 const day = now.getDay();
 
@@ -16,3 +20,17 @@ if (day != 1 && day != 2)
 {
 	document.querySelector("#add").style.display = "none";
 }
+
+var countDays = 0;
+
+if (localStorage.visit) {
+    countDays = (now.getTime() - new Date(localStorage.visit).getTime())/(1000*60*60*24);
+	console.log(defdate, countDays, localStorage.visit);
+	if (localStorage.visit != now) {
+		localStorage.visit = now;
+	} 	
+  } else {
+    localStorage.visit = now;
+  }
+
+document.getElementById("lastVisit").textContent = `Days from last visit: ${Math.round(countDays)}`;
