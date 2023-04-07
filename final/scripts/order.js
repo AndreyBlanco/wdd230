c6688c5fedd95daaf56f84543d683df5b4a96093
andreyblanco <andygblanco@gmail.com>
2023-04-06 20:35:33 -0600

    form

--- final/scripts/order.js
+++ final/scripts/order.js
@@ -36,3 +36,53 @@ const displayProducts = (products) => {
       third.appendChild(option3);
     };
 }
+
+document.getElementById("subOrder").addEventListener('click', changeForms);
+
+function changeForms()
+{
+    const order = document.mixForm;
+    
+    if (order.fname.value == null || order.fname.value.length == 0 || /^\s+$/.test(order.fname.value))
+    {
+        alert("The First Name is required");
+    }
+    else if (order.email.value == null || order.email.value.length == 0 || /^\s+$/.test(order.email.value))
+    {
+        alert("The Email is required");
+    }         
+    else if ( !(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(order.email.value)))
+    {
+        alert("The Email is invalid");
+    }
+    else if (order.tel.value == null || order.tel.value.length == 0 || /^\s+$/.test(order.tel.value))
+    {
+        alert("The Phone is required");
+    }
+    else if ( !(/^\d{6,12}$/.test(order.tel.value)))
+    {
+        alert("The Phone is invalid");
+    }
+    else if (order.iOne.options[iOne.selectedIndex].value == order.iOne.options[iTwo.selectedIndex].value || order.iOne.options[iOne.selectedIndex].value == order.iOne.options[iThree.selectedIndex].value || order.iOne.options[iTwo.selectedIndex].value == order.iOne.options[iThree.selectedIndex].value )
+    {
+        alert("The ingredients choosen must to be different.");
+    }
+    else
+    {
+        let name = order.fname.value;
+        let email = order.email.value;
+        let phone = order.tel.value;
+        let one = order.iOne.options[iOne.selectedIndex].value;
+        let two = order.iOne.options[iTwo.selectedIndex].value;
+        let three = order.iOne.options[iThree.selectedIndex].value;
+        let desc = order.desc.textContent;
+        if (desc == "Please, add special instructions");
+        {
+            desc = "No special instructions added."
+        }
+        let formulario = document.getElementById('mixForm');
+        formulario.submit();
+    }
+}
+
+
